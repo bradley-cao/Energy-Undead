8919a4641fcbb091e82c4c3204ae6811470775f7
Bradley Cao <bradleycao@gmail.com>
2023-03-04 18:00:51 -0500

    stacking blocks

--- Assets/Scripts/StackGame.cs
+++ Assets/Scripts/StackGame.cs
@@ -11,6 +11,7 @@ public class StackGame : MonoBehaviour
     public GameObject Canvas;
 
     GameObject newblock;
+    public bool gameRunning = false;
     public Vector3 leftBound;
     public Vector3 rightBound;
     private float speed = 5F;
@@ -19,14 +20,17 @@ public class StackGame : MonoBehaviour
     Vector3 velocity;
     void Start()
     {
+        gameRunning = true;
         start = Time.time;
         newblock = Instantiate(blockPrefab, new Vector3(0, -250, 0), Quaternion.identity);
         newblock.transform.SetParent(Canvas.transform, false);
+        Debug.Log(newblock.transform.position);
         // make block continuouslly bounce left and right
     }
 
     void Update()
     {
+        RectTransform blockTransform = newblock.GetComponent<RectTransform>();
         float current = Time.time;
         float speedBuffer = (current-start)*0.05f; // increase speed by 0.1 every second
         speed = 5 + speedBuffer;
@@ -46,6 +50,16 @@ public class StackGame : MonoBehaviour
         else if (direction == "left")
         {
             newblock.transform.Translate(-1 * speed * Time.deltaTime, 0, 0);
+        }
+
+        // on spacebar press, create new block, set it as child of canvas, and set it to the position of the old block, and stop old block
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //instantiate new block above old block
+            var height = blockTransform.rect.height;
+            Vector3 newPos = new Vector3(0, blockTransform.anchoredPosition.y+height, 0);
+            newblock = Instantiate(blockPrefab, newPos, Quaternion.identity);
+            newblock.transform.SetParent(Canvas.transform, false);
         }
     }
 }
